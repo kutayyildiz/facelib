b6f80be20d12bbdf5b6c342bcc5a7ac818b7fb32
kutayyildiz <ktyldz@gmail.com>
2020-03-19 13:09:09 +0300

    minor change on docstring

--- facelib/facerec/__init__.py
+++ facelib/facerec/__init__.py
@@ -1,6 +1,7 @@
 """Face recognition.
 
 Includes face detection, facial landmark detection and facial feature extraction.
+Extra Dependencies: tflite-runtime(pip)
 """
 from .face_detection import HAARFaceDetector
 from .face_detection import LBPFaceDetector
@@ -8,3 +9,4 @@ from .face_detection import SSDFaceDetector
 from .feature_extraction import FeatureExtractor
 from .landmark_detection import LandmarkDetector
 from .pipeline import Pipeline
+
