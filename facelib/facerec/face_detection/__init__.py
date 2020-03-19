c0b5f1c6f2b0dc811f5bfc57fe38eb492c0a7dd0
kutayyildiz <ktyldz@gmail.com>
2020-03-19 13:17:37 +0300

    class name changed SSD->SSDFaceDetector

--- facelib/facerec/face_detection/__init__.py
+++ facelib/facerec/face_detection/__init__.py
@@ -1,4 +1,4 @@
 """Face detection."""
 from .cascade import HAARCascade as HAARFaceDetector
 from .cascade import LBPCascade as LBPFaceDetector
-from .ssd_quantized import SSD as SSDFaceDetector
+from .ssd import SSDFaceDetector
