ed6401c9c91a6cfb7b81b4a4fc857f93a6cbc696
kutayyildiz <kutayyildiz@gmail.com>
2020-03-22 19:43:38 +0300

    squarize + name(tflite) change

--- facelib/facerec/face_detection/ssd.py
+++ facelib/facerec/face_detection/ssd.py
@@ -23,21 +23,19 @@ class SSDFaceDetector:
 
     def __init__(self, input_img_channel='rgb', resize=None, tpu=False):
         if tpu:
-            name_model = 'ssd_int8_edgetpu'
+            name_model = 'ssd_int8_tpu'
             delegates = [load_delegate('libedgetpu.so.1')]
         else:
-            name_model = 'ssd_int8'
+            name_model = 'ssd_int8_cpu'
             delegates = []
         path_tflite = helper.get_path('face_detection', name_model)
         self.face_detection_inference = Interpreter(
-            model_path = str(path_tflite),
-            experimental_delegates=delegates
-        )
+            model_path=str(path_tflite),
+            experimental_delegates=delegates)
         self.face_detection_inference.allocate_tensors()
         self.index_input = self.face_detection_inference.get_input_details()[0]['index']
         self.index_boxes = self.face_detection_inference.get_output_details()[0]['index']
         self.index_probs = self.face_detection_inference.get_output_details()[2]['index']
-        
         assert input_img_channel in ['bgr', 'rgb'], 'Incorrect input_img_channel'
         self.input_img_channel = input_img_channel
 
@@ -48,7 +46,8 @@ class SSDFaceDetector:
         img = np.expand_dims(img, 0)
         return img
 
-    def _extract_boxes(self, boxes, probs):
+    @staticmethod
+    def _extract_boxes(boxes, probs):
         threshold = 1e-1
         mask = np.less(probs, 1.)
         probs = probs[mask]
@@ -63,8 +62,11 @@ class SSDFaceDetector:
         Parameters
         ----------
         img : ndarray
-            3d array with shape (112,112,3) with dtype `uint8`.
+            3d array with shape (height,width,3) with dtype `uint8`.
         """
+        original_shape = img.shape
+        img = self.squarize_img(img)
+        squared_shape = img.shape
         img = self._preprocess(img)
         self.face_detection_inference.set_tensor(self.index_input, img)
         self.face_detection_inference.invoke()
@@ -78,4 +80,20 @@ class SSDFaceDetector:
         fix = fix * [0., 1, 0, -1]
         fix = fix.reshape(-1, 2, 2)
         bboxes = bboxes + fix
+        bboxes = self.normalize_bboxes(bboxes, original_shape, squared_shape)
+        return bboxes
+
+    @staticmethod
+    def squarize_img(img):
+        height, width = img.shape[:2]
+        max_dim = max(height, width)
+        new_shape = (max_dim, max_dim, 3)
+        squarized = np.zeros(new_shape, dtype=img.dtype)
+        squarized[:height,:width] += img
+        return squarized
+
+    @staticmethod
+    def normalize_bboxes(bboxes, original_shape, squared_shape):
+        change_ratio = np.array(squared_shape[:2], dtype=bboxes.dtype) / original_shape[:2]
+        bboxes = bboxes * change_ratio
         return bboxes
